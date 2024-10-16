c7eb54bb192a0f9e09d2570dad8e382734321573
rafal winnik <rafalwinnik@o2.pl>
2024-10-16 13:24:15 +0200

    change database to sqlite3

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^b15zcq^01xhl-xagupsm4+*u(3m^7eh)gb57i@*j62m0h184_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookshop-qq10.onrender.com']
 
@@ -88,12 +88,13 @@ WSGI_APPLICATION = "bookshop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 
 # DATABASES = {
 #     'default': {
@@ -106,11 +107,11 @@ WSGI_APPLICATION = "bookshop.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:1234@localhost:5432/db_bookshop',
-        conn_max_age=600)
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://postgres:1234@localhost:5432/db_bookshop',
+#         conn_max_age=600)
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -141,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
